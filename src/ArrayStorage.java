324903c7082867f68c1b9645f005c0a8e9c42d14
denis <den.chet@gmail.com>
2022-08-11 22:55:20 +0300

    i<= size changed to i<size, except save(), updated delete(), updated get(), import java.util.UUID deleted

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,12 +1,11 @@
 import java.util.Arrays;
-import java.util.UUID;
 
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int size;
 
     void clear() {
-        for (int i = 0; i <= size; ++i) {
+        for (int i = 0; i < size; ++i) {
             storage[i] = null;
         }
     }
@@ -15,6 +14,7 @@ public class ArrayStorage {
         for (int i = 0; i <= size; ++i) {
             if (r.uuid == null) {
                 System.out.println("You didn't enter anything");
+                break;
             }
             storage[size] = r;
             size++;
@@ -23,28 +23,20 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        int indexForGet = -1;
-
-        for (int i = 0; i <= size; ++i) {
-            indexForGet = indexForGet + 1;
+        for (int i = 0; i < size; ++i) {
             if (storage[i].uuid == uuid) {
-                return storage[indexForGet];
-            }
-            if (indexForGet == size - 1) {
-                System.out.println("No such Resume in Array");
-                break;
+                return storage[i];
             }
         }
+        System.out.println("No such Resume in Array");
         return null;
     }
 
     void delete(String uuid) {
-        int j = size;
-
-        for (int i = 0; i < j; ++i) {
+        for (int i = 0; i < size; ++i) {
             if (storage[i].uuid == uuid) {
-                storage[i] = storage[j - 1];
-                storage[j - 1] = null;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 break;
             }
         }
